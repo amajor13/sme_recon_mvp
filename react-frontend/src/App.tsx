9f7cb489cdafb8f175cf0c4b1d59bcbe2e0f2df3
ashutosh <am18th@gmail.com>
2025-09-30 00:25:44 +0530

    Hide Auth0 debugger in production frontend

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -3,16 +3,18 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './components/Dashboard';
 import { Toaster } from '@/components/ui/toaster';
 
-// Import the Auth0Debugger component
-import Auth0Debugger from './components/Auth0Debugger';
+// Import the Auth0Debugger component (dev only)
+// import Auth0Debugger from './components/Auth0Debugger';
 
 function App() {
   return (
     <AuthProvider>
-      {/* Auth0 Debug Panel */}
+      {/* Auth0 Debug Panel - disabled for production */}
+      {/* 
       <div className="fixed top-2 right-2 left-2 z-50 max-w-2xl mx-auto">
         <Auth0Debugger />
       </div>
+      */}
       
       <ProtectedRoute>
         <Dashboard />
